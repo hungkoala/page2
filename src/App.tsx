d42b9efef41d50b386f81f870802eb156c8ecef1
Hung Nguyen <hunge@zoop.care>
2023-09-04 16:50:51 +0700

    test4

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>This is the test page</h1>
+      <h1>This is the test page2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
